d1e3f8194673adcc6937ea09bec5ca11f2a91613
origin <492802205@qq.com>
2016-10-09 17:13:26 +0800

    初始化TextView

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.administrator.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView mTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView =  (TextView) findViewById(R.id.tv);
     }
 }
